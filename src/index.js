33755b9ffa4b57f3e07a5eaea29d9966f0171df6
Joann Pan <joannlpan@gmail.com>
2021-09-18 16:45:26 -0400

    installed packages and imported packages to package.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +12,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
