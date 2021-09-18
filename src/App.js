11f13e8ae8a6a1d97e903622a8cba3ff0715c07c
Joann Pan <joannlpan@gmail.com>
2021-09-18 14:24:54 -0400

    add fetch request to test backend data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class App extends React.Component{
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/accounts') 
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+
+  }
+
+
+
   render() {
     return (
       <div>
