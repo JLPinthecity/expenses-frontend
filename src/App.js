acd9e949918d0b6e7a64d91cfee0f00af3c1e7e9
Joann Pan <joannlpan@gmail.com>
2021-09-20 10:33:09 -0400

    create accounts container and import to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchAccounts} from './actions/fetchAccounts'
+import AccountsContainer from './containers/AccountsContainer';
 
 class App extends React.Component{
 
@@ -8,18 +8,16 @@ class App extends React.Component{
     this.props.fetchAccounts({type: 'FETCH_ACCOUNTS', payload: {name: 'Checking'}} )
   }
 
-
-
   render() {
     return (
       <div>
-        App
+        <AccountsContainer/>
       </div>
     );
   };
 };
 
-export default connect(null, {fetchAccounts})(App);
+export default connect()(App);
 
 //mapStateToProps gives us access to the store 
 //mapDispatchToProps gives us the ability to dispatch new actions from component 
