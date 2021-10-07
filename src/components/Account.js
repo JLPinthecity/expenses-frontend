392ce50c2cdc5b9fac44b6bf2282c02002bcd021
Joann Pan <joannlpan@gmail.com>
2021-10-07 16:41:24 -0400

    set up account component and import into accounts componentt

---
+++ src/components/Account.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Account = (props) => {
+
+
+}
+
+export default Account
--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Account from './Account'
 
 const Accounts = ({accounts}) => {
 
     return (
         <div>
-            {accounts.map(account => <li key={account.id}>{account.name} = {account.balance} </li>)}
+            {accounts.map(account => <li key={account.id}>{account.name} - {account.balance} </li>)}
         </div>
     )
 }
