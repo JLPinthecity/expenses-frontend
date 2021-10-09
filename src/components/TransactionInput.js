b5fff66cfd088a33010b1930ae7d4f6814b381ff
Joann Pan <joannlpan@gmail.com>
2021-10-09 16:04:25 -0400

    add transaction form

--- src/components/TransactionInput.js
+++ src/components/TransactionInput.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 class TransactionInput extends React.Component {
 
     render() {
         return (
             <div>
-                TransactionInput
+                <form>
+                    <label>
+                        Transaction Type:
+                    </label>
+                    <select>
+                        <option>deposit</option>
+                        <option>withdrawal</option>
+                    </select>
+                    <label>
+                        Transaction Amount:
+                    </label>
+                    <input type="text"/>
+                </form>
             </div>
         )
     }
 }
 
-export default TransactionInput;
+export default connect()(TransactionInput);
