9887b23fc002c412567717a952dc7d86183c6775
Joann Pan <joannlpan@gmail.com>
2021-10-11 10:48:25 -0400

    have delete transaction controller retrieve account from backend to simplify work in reducer

--- src/actions/deleteTransaction.js
+++ src/actions/deleteTransaction.js
@@ -1,4 +1,11 @@
-export const deleteTransaction = () => {
+export const deleteTransaction = (transactionId) => {
 
-    
+    return (dispatch) => {
+        return fetch(`http://localhost:3000/api/v1/accounts/${accountId}/transactions/${transactionId}`, {
+            method: 'DELETE', 
+        })
+        .then(response => response.json())
+        .then(account => dispatch({type: 'DELETE_TRANSACTION', payload: account}))
+    }
+
 }
