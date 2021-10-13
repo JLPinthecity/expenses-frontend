5a18fb3c02a9a61e9c5a9e86b56c4737c849344e
Joann Pan <joannlpan@gmail.com>
2021-10-13 15:10:21 -0400

    add case in reducer for delete transaction

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -17,6 +17,17 @@ export default function accountReducer(state = {accounts: []}, action) {
             })
 
             return {...state, accounts: accounts}
+        
+        case 'DELETE_TRANSACTION':
+            let accountsTwo = state.accounts.map(account => {
+                if (account.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return account
+                }
+            })
+
+            return {...state, accounts: accountsTwo}
 
         default:
             return state;
